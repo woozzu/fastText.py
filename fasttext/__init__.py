345f146e8e26421eac6033aa5b09bbc053fd415c
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2016-08-29 14:23:36 +0700

    Set fasttext.__VERSION__

--- fasttext/__init__.py
+++ fasttext/__init__.py
@@ -3,7 +3,13 @@ from .fasttext import cbow
 from .fasttext import load_model
 from .fasttext import supervised
 
-@property
-def __VERSION__():
-    with open('VERSION') as f:
+import os
+
+dir_path = os.path.dirname(os.path.realpath(__file__))
+version_path = os.path.join(dir_path, 'VERSION')
+
+def _read_version():
+    with open(version_path) as f:
         return f.read().strip()
+
+__VERSION__ = _read_version()
