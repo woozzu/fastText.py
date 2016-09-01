339e33acd09d30cecb23eba334a68b7c3ed880d4
Bayu Aldi Yansyah <bayualdiyansyah@gmail.com>
2016-09-01 23:57:23 +0700

    Remove fasttext/VERSION; it cause unexpected behaviour in different env

--- fasttext/__init__.py
+++ fasttext/__init__.py
@@ -5,11 +5,4 @@ from .fasttext import supervised
 
 import os
 
-dir_path = os.path.dirname(os.path.realpath(__file__))
-version_path = os.path.join(dir_path, 'VERSION')
-
-def _read_version():
-    with open(version_path) as f:
-        return f.read().strip()
-
-__VERSION__ = _read_version()
+__VERSION__ = '0.7.5'
